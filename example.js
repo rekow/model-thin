0d276cf586ca10c487c85ee4744a013dd87b639b
David Rekow <d@davidrekow.com>
2015-07-24 14:42:06 -0700

    Add model kinds, get rid of extraneous Property.

--- example.js
+++ example.js
@@ -4,7 +4,7 @@
 
 var Model = require('./index');
 
-var Person = Model.create({name: String, age: Number});
+var Person = Model.create('Parent', {name: String, age: Number});
 Person.defineProperty('parent', Person); // Required for recursive typing.
 
 var father = new Person();
@@ -24,3 +24,16 @@ console.log('Father age: ' + father.age);
 
 console.log('Son.parent name: ' + son.parent.name);
 console.log('Son.parent age: ' + son.parent.age);
+
+// Type validation done at set time.
+son.name = 17;
+console.log('Son name: ' + son.name);
+
+son.age = 'Ron';
+console.log('Son age: ' + son.age);
+
+// Setting null deletes, passes type check (won't be true once required is implemented)
+son.name = son.age = null;
+
+console.log('Son name: ' + son.name);
+console.log('Son age: ' + son.age);
--- index.js
+++ index.js
@@ -1,52 +1,28 @@
 /**
  * @file Super skinny model class.
+ * @author David Rekow <d@davidrekow.com>
  */
 
-var Property, Model;
 
-Property = function (type) {
-  this._type = type;
-  this._value = null;
-};
-
-Property.prototype.validate = function (val) {
-  return val.constructor === this._type;
-};
-
-
-Model = function () {
+/**
+ * @constructor
+ */
+var Model = function () {
+  /**
+   * @private
+   * @type {Object.<string, ?>}
+   */
   this._prop = {};
 };
 
-Model.prototype.validate = function () {
-  // @TODO model-level validations (required, etc).
-};
-
-Model.bind = function (ctor, key, type) {
-  Object.defineProperty(ctor.prototype, key, {
-    enumerable: true,
-    get: function () {
-      if (!this._prop[key]) {
-        this._prop[key] = new Property(type);
-      }
-
-      return this._prop[key]._value;
-    },
-    set: function (val) {
-      if (!this._prop[key]) {
-        this._prop[key] = new Property(type);
-      }
-
-      if (this._prop[key].validate(val)) {
-        this._prop[key]._value = val;
-      } else {
-        console.warn('Tried to set invalid property type, ignoring.');
-      }
-    }
-  });
-};
-
-Model.create = function (props, parent) {
+/**
+ * @static
+ * @param {string} kind
+ * @param {Object.<string, function(new:?)>} props
+ * @param {function(new:Model)=} parent
+ * @return {function(new:Model)}
+ */
+Model.create = function (kind, props, parent) {
   parent = parent || Model;
 
   var ctor = function () {
@@ -55,10 +31,10 @@ Model.create = function (props, parent) {
 
   ctor.prototype = Object.create(parent.prototype);
   ctor.prototype.constructor = ctor;
+  ctor.prototype.kind = kind;
+  ctor.prototype.props = {};
 
-  ctor.defineProperty = function (key, type) {
-    Model.bind(this, key, type);
-  };
+  ctor.defineProperty = Model.defineProperty;
 
   for (var key in props) {
     if (props.hasOwnProperty(key)) {
@@ -68,5 +44,38 @@ Model.create = function (props, parent) {
 
   return ctor;
 };
+
+/**
+ * @static
+ * @param {string} key
+ * @param {function(new:?)} type
+ * @this {function(new:Model)}
+ */
+Model.defineProperty = function (key, type) {
+  Object.defineProperty(this.prototype, key, {
+    enumerable: true,
+    get: function () {
+      return this._prop[key];
+    },
+    set: function (val) {
+      if (val !== null && val.constructor !== this.props[key]) {
+        console.warn('Tried to set invalid property type, ignoring.');
+      } else {
+        this._prop[key] = val;
+      };
+    }
+  });
+
+  this.prototype.props[key] = type;
+};
+
+/**
+ * @return {boolean}
+ * @TODO model-level validations (required, indexed etc).
+ */
+Model.prototype.validate = function () {
+  return true;
+};
+
 
 module.exports = Model;
